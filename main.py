a7875f595a846933c4226f554c6a58dc9b00b463
vlouggos <44415066+vlouggos@users.noreply.github.com>
2022-12-29 02:50:56 +0200

    6

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+import os
+from postgres import Postgres
+import psycopg2
 
 app = Flask(__name__)
+DATABASE_URL = os.environ['DATABASE_URL']
+print(DATABASE_URL)
+
+
 
 @app.route('/')
 def home():
