bca749d3e02b8b0c8fe674384a57f89ee8d177ce
vlouggos <44415066+vlouggos@users.noreply.github.com>
2022-12-29 18:19:17 +0200

    Major update

--- main.py
+++ main.py
@@ -15,26 +15,41 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 ##CREATE TABLE
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    author = db.Column(db.String(250), nullable=False)
-    rating = db.Column(db.Float, nullable=False)
-
-    # Optional: this will allow each book object to be identified by its title when printed.
-    def __repr__(self):
-        return f'<Book {self.title}>'
-
-db.create_all()
-
-
 @app.route('/')
 def home():
-    new_book = Book(title="title1", author="author1", rating=10)
-    db.session.add(new_book)
-    db.session.commit()
-    book_to_print = Book.title
-    return render_template("index.html", alpha="book_to_print")
+    ##READ ALL RECORDS
+    all_books = db.session.query(Book).all()
+    return render_template("index.html", books=all_books)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add():
+    if request.method == "POST":
+       new_book = Book(title=request.form["title"], author=request.form["author"], rating=request.form["rating"])
+       db.session.add(new_book)
+       db.session.commit()
+       return redirect(url_for('home'))
+    return render_template("add.html")
+
+@app.route('/rating', methods=["GET", "POST"])
+def rating():
+        if request.method == "POST":
+            book_id = request.form["id"]
+            book_to_update = Book.query.get(book_id)
+            book_to_update.rating = request.form["rating"]
+            db.session.commit()
+            return redirect(url_for('home'))
+        book_id = request.args.get('id')
+        book_selected = Book.query.get(book_id)
+        return render_template("rating.html", book=book_selected)
+
+@app.route('/delete')
+def delete():
+        book_id = request.args.get('id')
+        book_to_delete = Book.query.get(book_id)
+        db.session.delete(book_to_delete)
+        db.session.commit()
+        return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
