36d917902a10fb8490c531aa67a6cfe60cfffc09
vlouggos <44415066+vlouggos@users.noreply.github.com>
2022-12-30 11:08:35 +0200

    Major update 10

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import psycopg2
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
+#app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
 
 #CONNECT TO DB
 app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
@@ -24,34 +24,34 @@ def home():
     return render_template("index.html")#, books=all_books#
 
 
-@app.route("/add", methods=["GET", "POST"])
-def add():
-    if request.method == "POST":
-       new_book = Book(title=request.form["title"], author=request.form["author"], rating=request.form["rating"])
-       db.session.add(new_book)
-       db.session.commit()
-       return redirect(url_for('home'))
-    return render_template("add.html")
-
-@app.route('/rating', methods=["GET", "POST"])
-def rating():
-        if request.method == "POST":
-            book_id = request.form["id"]
-            book_to_update = Book.query.get(book_id)
-            book_to_update.rating = request.form["rating"]
-            db.session.commit()
-            return redirect(url_for('home'))
-        book_id = request.args.get('id')
-        book_selected = Book.query.get(book_id)
-        return render_template("rating.html", book=book_selected)
-
-@app.route('/delete')
-def delete():
-        book_id = request.args.get('id')
-        book_to_delete = Book.query.get(book_id)
-        db.session.delete(book_to_delete)
-        db.session.commit()
-        return redirect(url_for('home'))
+#@app.route("/add", methods=["GET", "POST"])
+#def add():
+#    if request.method == "POST":
+#       new_book = Book(title=request.form["title"], author=request.form["author"], rating=request.form["rating"])
+#       db.session.add(new_book)
+#       db.session.commit()
+#       return redirect(url_for('home'))
+#    return render_template("add.html")
+#
+#@app.route('/rating', methods=["GET", "POST"])
+#def rating():
+#        if request.method == "POST":
+#            book_id = request.form["id"]
+#            book_to_update = Book.query.get(book_id)
+#            book_to_update.rating = request.form["rating"]
+#            db.session.commit()
+#            return redirect(url_for('home'))
+#        book_id = request.args.get('id')
+#        book_selected = Book.query.get(book_id)
+#        return render_template("rating.html", book=book_selected)
+#
+#@app.route('/delete')
+#def delete():
+#        book_id = request.args.get('id')
+#        book_to_delete = Book.query.get(book_id)
+#        db.session.delete(book_to_delete)
+#        db.session.commit()
+#        return redirect(url_for('home'))
 
 if __name__ == "__main__":
     app.run(debug=True)
